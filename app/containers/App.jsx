28ed9a92a3ded78812114419f729296321828470
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-05-30 22:07:05 +0200

    Add basic MD resets and styles

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,14 +1,48 @@
 // @flow
 import * as React from "react";
+import {withStyles} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 type Props = {
+  classes: Object,
   children: React.Node
 };
 
-export default class App extends React.Component<Props> {
+const styles = theme => ({
+  root: {
+    display: "flex",
+    alignItems: "stretch",
+    width: "100%"
+  },
+  grow: {
+    flex: "1 1 auto"
+  }
+});
+
+class App extends React.Component<Props> {
   props: Props;
 
   render() {
-    return this.props.children;
+    const {classes} = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              Foodie
+            </Typography>
+
+            <div className={classes.grow} />
+          </Toolbar>
+        </AppBar>
+
+        {this.props.children}
+      </div>
+    );
   }
 }
+
+export default withStyles(styles)(App);
--- app/routes.jsx
+++ app/routes.jsx
@@ -1,12 +1,27 @@
 import * as React from "react";
 import {Switch, Route} from "react-router";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import amber from "@material-ui/core/colors/amber";
+import lightGreen from "@material-ui/core/colors/lightGreen";
+import {MuiThemeProvider, createMuiTheme} from "@material-ui/core/styles";
 import App from "./containers/App";
 import Home from "./containers/Home";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: lightGreen,
+    secondary: amber
+  }
+});
+
 export default () => (
-  <App>
-    <Switch>
-      <Route path="/" exact component={Home} />
-    </Switch>
-  </App>
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+
+    <App>
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </App>
+  </MuiThemeProvider>
 );
