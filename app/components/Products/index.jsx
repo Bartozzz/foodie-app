3ffd3842d15d811741abb42c8381f2cd99882fa0
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-06-06 15:14:42 +0200

    Implement view mode change

--- app/components/Products/index.jsx
+++ app/components/Products/index.jsx
@@ -1,10 +1,18 @@
 // @flow
 import * as React from "react";
+import {withStyles} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import ViewListIcon from "@material-ui/icons/ViewList";
+import ViewModuleIcon from "@material-ui/icons/ViewModule";
 import ProductList from "./List";
 import ProductTable from "./Table";
 import type {Product} from "../../types/off/Product";
 
 type ComponentProps = {
+  classes: Object,
   products: Array<Product>,
   brand: string,
   count: number,
@@ -22,13 +30,71 @@ class Products extends React.Component<ComponentProps, ComponentState> {
     mode: "TABLE"
   };
 
-  render() {
+  changeMode = (mode: string) => (event: Object) => {
+    this.setState({
+      mode
+    });
+  };
+
+  renderMode(mode: string) {
+    const {onSelect, onChangePage, page, count, brand, products} = this.props;
+    const props = {onSelect, onChangePage, page, count, brand, products};
+
+    switch (mode) {
       case "LIST":
-        return <ProductList {...this.props} />;
+        return <ProductList {...props} />;
       default:
-        return <ProductTable {...this.props} />;
+        return <ProductTable {...props} />;
     }
+  }
+
+  render() {
+    const {classes, count} = this.props;
+    const {mode} = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="secondary">
+          <Toolbar>
+            <Typography variant="subheading" color="inherit">
+              Found {count} results.
+            </Typography>
+
+            <div className={classes.grow} />
+
+            <IconButton onClick={this.changeMode("LIST")}>
+              <ViewListIcon />
+            </IconButton>
+
+            <IconButton onClick={this.changeMode("TABLE")}>
+              <ViewModuleIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
+        <div className={classes.content}>{this.renderMode(mode)}</div>
+      </div>
+    );
   }
 }
 
-export default Products;
+export default withStyles((theme: Object) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+
+    width: "100%",
+    height: "100%"
+  },
+
+  grow: {
+    flex: "1"
+  },
+
+  content: {
+    flex: "1",
+
+    overflowX: "hidden",
+    overflowY: "auto"
+  }
+}))(Products);
